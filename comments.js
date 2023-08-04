9ec6e40276a0ec04e67de5f9da15261c640a928a
RUbikS3ri0uS <141409266+RUbikS3ri0uS@users.noreply.github.com>
2023-08-04 11:05:08 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,18 @@
+// Create web Server
+// 1. npm install express
+// 2. npm install body-parser
+// 3. npm install cookie-parser
+// 4. npm install express-session
+// 5. npm install mysql
+// 6. npm install ejs
+// 7. npm install mysql
+// 8. npm install express-myconnection
+// 9. npm install express-error-handler
+// 10. npm install express-logger
+// 11. npm install multer
+// 12. npm install morgan
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
